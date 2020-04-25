76de6ed6a760ed3d5d448c83ca5a2338cc1806d5
Rafael <rafael007@gmail.com>
2020-04-24 19:38:42 -0500

    Added option and radio button validation

--- login.php
+++ login.php
@@ -13,10 +13,23 @@
         <form action="login.php" method="POST">
             <?php
 
+            $nombre = "";
+            $password = "";
+            $email = "";
+            $pais = "";
+            $nivel = "";
+
             if(isset($_POST['nombre'])){
-            $nombre = $_POST['nombre'];
-            $email = $_POST['user'];
-            $password = $_POST['password'];
+                $nombre = $_POST['nombre'];
+                $email = $_POST['user'];
+                $password = $_POST['password'];
+                $pais = $_POST['pais'];
+            
+            if(isset($_POST['nivel'])){
+                $nivel = $_POST['nivel'];
+            }else{
+                $nivel = "";
+            }
 
             $campos = array();
 
@@ -32,13 +45,21 @@
                 array_push($campos, "Debes ingresar un correo electrónico válido");
             }
 
+            if($pais == ""){
+                array_push($campos, "Selecciona un país de la lista");
+            }
+
+            if($nivel == ""){
+                array_push($campos, "Selecciona un nivel de desarrollo");
+            }
+
             if(count($campos) > 0){
                 echo "<div class='error'>";
                 for($i = 0; $i < count($campos);$i++){
                 echo "<li>".$campos[$i]."</li>";
             }
             }else{
-                echo "<div class='correcto'>Datos correctos";
+                echo "<div class='correcto'>Datos enviados correctamente";
             }
             echo "</div>";
             }
@@ -47,15 +68,39 @@
 
             <h2>LOGIN</h2>
             <div>
-                <input type="text" name="nombre" placeholder="Nombre completo">
+                <input type="text" name="nombre" placeholder="Nombre completo" value="<?php echo $nombre; ?>">
             </div>
             <br>
             <div>
-                <input type="text" name="user" placeholder="Nombre de usuario">
+                <input type="text" name="user" placeholder="Nombre de usuario" value="<?php echo $email; ?>">
             </div>
             <br>
             <div>
-                <input type="password" name="password" placeholder="Password">
+                <input type="password" name="password" placeholder="Password" value="<?php echo $password; ?>">
+            </div>
+
+            <div>
+                <h5>País de Origen: </h5>
+                <select name="pais" id="">
+                    <option value="">Selecciona un país...</option>
+                    <option value="col" <?php if($pais == "col") echo "selected" ?>>Colombia</option>
+                    <option value="chi" <?php if($pais == "chi") echo "selected" ?>>Chile</option>
+                    <option value="ned" <?php if($pais == "ned") echo "selected" ?>>Holanda</option>
+                    <option value="esp" <?php if($pais == "esp") echo "selected" ?>>España</option>
+                    <option value="jap" <?php if($pais == "jap") echo "selected" ?>>Japón</option>
+                    <option value="arg" <?php if($pais == "arg") echo "selected" ?>>Argentina</option>
+                </select>
+            </div>
+            <br>
+
+            <div>
+                <h5>Nivel de desarrollador: </h5>
+                <input type="radio" name="nivel" value="entry_level"
+                    <?php if($nivel == "entry_level") echo "checked"; ?>>Entry level
+                <input type="radio" name="nivel" value="junior" <?php if($nivel == "junior") echo "checked"; ?>>Junior
+                <input type="radio" name="nivel" value="senior" <?php if($nivel == "senior") echo "checked"; ?>>Senior
+                <input type="radio" name="nivel" value="arquitect"
+                    <?php if($nivel == "arquitect") echo "checked"; ?>>Arquitecto
             </div>
             <br>
             <div>
